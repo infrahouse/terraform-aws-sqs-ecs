2924e19e38fda1777faffcda7f90a4299599c1f0
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-06-09 11:18:45 -0700

    Try workaround for https://github.com/hashicorp/terraform-provider-aws/issues/39584

--- modules/ecs/service.tf
+++ modules/ecs/service.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_service" "consumer" {
   lifecycle {
     ignore_changes = [
       desired_count,
-      capacity_provider_strategy  # workaround for https://github.com/hashicorp/terraform-provider-aws/issues/39584
+      capacity_provider_strategy # workaround for https://github.com/hashicorp/terraform-provider-aws/issues/39584
     ]
   }
 
