eac4a9185d84de5f1849c453959a02c9c3138361
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-06-11 09:43:50 -0400

    Tune module dependencies
    The ECS part of the module depends on the ASG security groups.
    Without them, the ECS agent fails to deregegister containers when the cluster/service is being deleted.

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -2,3 +2,11 @@ output "asg_arn" {
   description = "ARN of the created autoscaling group."
   value       = aws_autoscaling_group.consumer.arn
 }
+
+# These outputs are needed to inject dependencies into the ECS module
+output "security_group_inbound_rule" {
+  value = aws_vpc_security_group_ingress_rule.icmp.id
+}
+output "security_group_outbound_rule" {
+  value = aws_vpc_security_group_egress_rule.default.id
+}
