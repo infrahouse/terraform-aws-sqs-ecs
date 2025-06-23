2f9aac4175dddb36978edd1334f1bd86af6468aa
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-06-23 12:50:49 -0700

    Add ChangeMessageVisibility permission to task role (#11)
    * Add ChangeMessageVisibility permission to task role
    
    * Ensure ondemand instance for reliable test

--- test_data/sql-ecs/main.tf
+++ test_data/sql-ecs/main.tf
@@ -3,12 +3,11 @@ resource "random_string" "suffix" {
   special = false
 }
 module "test" {
-  source                           = "./../../"
-  service_name                     = "sqs-test-${random_string.suffix.result}"
-  consumer_subnet_ids              = var.consumer_subnet_ids
-  consumer_on_demand_base_capacity = 0
-  consumer_docker_image            = "httpd"
-  consumer_asg_max_size            = 1
-  consumer_asg_min_size            = 1
-  consumer_instance_type           = "t3a.micro"
+  source                 = "./../../"
+  service_name           = "sqs-test-${random_string.suffix.result}"
+  consumer_subnet_ids    = var.consumer_subnet_ids
+  consumer_docker_image  = "httpd"
+  consumer_asg_max_size  = 1
+  consumer_asg_min_size  = 1
+  consumer_instance_type = "t3a.micro"
 }
