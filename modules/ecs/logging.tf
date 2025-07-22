ff1697d5a9302ddfcb6541ff3f84cadcccd6415a
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-07-22 13:57:51 -0700

    Create container insights log group

--- modules/ecs/logging.tf
+++ modules/ecs/logging.tf
@@ -9,3 +9,15 @@ resource "aws_cloudwatch_log_group" "consumer" {
     }
   )
 }
+
+resource "aws_cloudwatch_log_group" "containerinsights" {
+  name              = "/aws/ecs/containerinsights/${aws_ecs_cluster.consumer.name}/performance"
+  retention_in_days = var.cloudwatch_log_group_retention
+  tags = merge(
+    local.default_module_tags,
+    {
+      VantaContainsUserData : false
+      VantaContainsEPHI : false
+    }
+  )
+}
